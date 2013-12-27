3a6f59ef09f1e711c496c6a9cac842f30f3631e5
Mitchell Vanderhoeff <mitchellvanderhoeff@gmail.com>
2013-12-27 12:26:06 +0100

    Implemented some user group routes.

--- app/controllers/Person.scala
+++ app/controllers/Person.scala
@@ -7,33 +7,65 @@ import storage.{BaseXConnection, BaseXBridge}
 
 object Person extends Controller with BaseXBridge {
 
-  //  app.get('/person/user/select', function (req, res) {
-  //  app.get('/person/user/all', function (req, res) {
-  //  app.get('/person/group/fetch/:identifier', function (req, res) {
-  //  app.get('/person/group/select', function (req, res) {
-  //  app.get('/person/group/all', function (req, res) {
-  //  app.post('/person/group/save', function (req, res) {
-  //  app.get('/person/group/:identifier/users', function (req, res) {
-  //  app.post('/person/group/:identifier/add', function (req, res) {
-  //  app.post('/person/group/:identifier/remove', function (req, res) {
+	//  app.get('/person/group/all', function (req, res) {
+	//  app.post('/person/group/save', function (req, res) {
+	//  app.get('/person/group/:identifier/users', function (req, res) {
+	//  app.post('/person/group/:identifier/add', function (req, res) {
+	//  app.post('/person/group/:identifier/remove', function (req, res) {
 
-  def getUser(identifier: String) = Action(
-    BaseXConnection.withSession(
-      session =>
-        session.findOneRaw(userPath(identifier)) match {
-          case Some(xml) => Ok(xml)
-          case None => NotFound
-        }
-    )
-  )
-  
-  def getAllUsers = Action(
-    BaseXConnection.withSession(
-      session => {
-        val xmlList = session.find(userCollection).toList
-        Ok(<Users>{ for (user <- xmlList) yield user }</Users>)
-      }
-    )
-  )
+
+	//  app.get('/person/user/select', function (req, res) {
+	def getUser(identifier: String) = Action(
+		BaseXConnection.withSession(
+			session =>
+				session.findOne(userPath(identifier)) match {
+					case Some(xml) => Ok(xml)
+					case None => NotFound
+				}
+		)
+	)
+
+
+	//  app.get('/person/user/all', function (req, res) {
+	def getAllUsers = Action(
+		BaseXConnection.withSession(
+			session => {
+				val xmlList = session.find(userCollection).toList
+				Ok(
+					<Users>
+						{for (user <- xmlList) yield user}
+					</Users>
+				)
+			}
+		)
+	)
+
+	//  app.get('/person/group/fetch/:identifier', function (req, res) {
+	def fetchGroup(identifier: String) = Action {
+		BaseXConnection.withSession {
+			session =>
+			  session.findOne(groupPath(identifier)) match {
+				  case Some(xml) => Ok(xml)
+				  case None => NotFound
+			  }
+		}
+	}
+
+	//  app.get('/person/group/select', function (req, res) {
+	def selectGroup(q: String) = Action {
+		BaseXConnection.withSession {
+			session =>
+			  	val query =
+					<Groups>
+						{{ {groupCollection}[contains(lower-case(Name), lower-case({quote(q.toLowerCase)}))] }}
+					</Groups>
+
+				session.findOne(query.toString()) match {
+					case Some(xml) => Ok(xml)
+					case None => NotFound
+				}
+		}
+	}
+
 }
 
--- app/storage/BaseX.scala
+++ app/storage/BaseX.scala
@@ -4,101 +4,107 @@ import eu.delving.basex.client.BaseX
 import org.basex.server.ClientSession
 
 object BaseXConnection {
-  lazy val server = new BaseX(host = "localhost", port = 1984, eport = 2013, user = "admin", pass = "admin")
+	lazy val server = new BaseX(host = "localhost", port = 1984, eport = 2013, user = "admin", pass = "admin")
 
-  def withSession[T](block: ClientSession => T) = {
-    server.withSession("oscr") {
-      session =>
-        block(session)
-    }
-  }
+	def withSession[T](block: ClientSession => T) = {
+		server.withSession("oscr") {
+			session =>
+				block(session)
+		}
+	}
 }
 
 trait BaseXBridge {
 
 
-  val database = "oscr"
+	val database = "oscr"
 
-  def langDocument(lang: String) = {
-    s"/i18n/$lang.xml"
-  }
+	def langDocument(lang: String) = {
+		s"/i18n/$lang.xml"
+	}
 
-  def langPath(lang: String): String = s"doc('$database${langDocument(lang)}')/Language"
+	def langPath(lang: String): String = s"doc('$database${langDocument(lang)}')/Language"
 
-  def groupCollection =  s"collection('$database/people/groups')/Group"
+	def groupCollection = s"collection('$database/people/groups')/Group"
 
-  def docCollection(schemaName: String) = s"collection('$database/documents/$schemaName')"
+	def docCollection(schemaName: String) = s"collection('$database/documents/$schemaName')"
 
-  def userCollection = s"collection('$database/people/users')/User"
+	def userCollection = s"collection('$database/people/users')/User"
 
-  def userDocument(identifier: String) = s"/people/users/$identifier.xml"
+	def userDocument(identifier: String) = s"/people/users/$identifier.xml"
 
-  def userPath(identifier: String) = s"doc('$database${userDocument(identifier)}')/User"
+	def userPath(identifier: String) = s"doc('$database${userDocument(identifier)}')/User"
 
-  //  this.groupDocument = function (identifier) {
-  //    return "/people/groups/" + identifier + ".xml";
-  //  };
-  //
-  //  this.groupPath = function (identifier) {
-  //    return "doc('" + this.database + this.groupDocument(identifier) + "')/Group";
-  //  };
-  //
-  //
-  //  this.schemaPath = function () {
-  //    return "doc('" + this.database + "/Schemas.xml')/Schemas";
-  //  };
-  //
-  //  this.vocabDocument = function (vocabName) {
-  //    return "/vocabulary/" + vocabName + ".xml";
-  //  };
-  //
-  //  this.vocabPath = function (vocabName) {
-  //    return "doc('" + this.database + this.vocabDocument(vocabName) + "')";
-  //  };
-  //
-  //  this.vocabExists = function (vocabName) {
-  //    return "db:exists('" + this.database + "','" + this.vocabDocument(vocabName) + "')";
-  //  };
-  //
-  //  this.vocabAdd = function (vocabName, xml) {
-  //    return "db:add('" + this.database + "', " + xml + ",'" + this.vocabDocument(vocabName) + "')";
-  //  };
-  //
-  //  this.docDocument = function (schemaName, identifier) {
-  //    if (!schemaName) throw new Error("No schema name!");
-  //    if (!identifier) throw new Error("No identifier!");
-  //    return "/documents/" + schemaName + "/" + identifier + ".xml";
-  //  };
-  //
-  //  this.docPath = function (schemaName, identifier) {
-  //    return "doc('" + this.database + this.docDocument(schemaName, identifier) + "')/Document";
-  //  };
-  //
-  //  this.logDocument = function () {
-  //    var now = new Date();
-  //    return "/log/" + now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + ".xml";
-  //  };
-  //
-  //  this.logPath = function () {
-  //    return "doc('" + this.database + this.logDocument() + "')";
-  //  };
+	def groupDocument(identifier: String) = s"/people/groups/$identifier.xml"
+
+	def groupPath(identifier: String) = s"doc('$database${groupDocument(identifier)}')/Group"
+
+	def groupCollection(identifier: String) = s"collection('$database/people/groups')/Group"
+
+	//  this.groupDocument = function (identifier) {
+	//    return "/people/groups/" + identifier + ".xml";
+	//  };
+	//
+	//  this.groupPath = function (identifier) {
+	//    return "doc('" + this.database + this.groupDocument(identifier) + "')/Group";
+	//  };
+	//
+	//
+	//  this.schemaPath = function () {
+	//    return "doc('" + this.database + "/Schemas.xml')/Schemas";
+	//  };
+	//
+	//  this.vocabDocument = function (vocabName) {
+	//    return "/vocabulary/" + vocabName + ".xml";
+	//  };
+	//
+	//  this.vocabPath = function (vocabName) {
+	//    return "doc('" + this.database + this.vocabDocument(vocabName) + "')";
+	//  };
+	//
+	//  this.vocabExists = function (vocabName) {
+	//    return "db:exists('" + this.database + "','" + this.vocabDocument(vocabName) + "')";
+	//  };
+	//
+	//  this.vocabAdd = function (vocabName, xml) {
+	//    return "db:add('" + this.database + "', " + xml + ",'" + this.vocabDocument(vocabName) + "')";
+	//  };
+	//
+	//  this.docDocument = function (schemaName, identifier) {
+	//    if (!schemaName) throw new Error("No schema name!");
+	//    if (!identifier) throw new Error("No identifier!");
+	//    return "/documents/" + schemaName + "/" + identifier + ".xml";
+	//  };
+	//
+	//  this.docPath = function (schemaName, identifier) {
+	//    return "doc('" + this.database + this.docDocument(schemaName, identifier) + "')/Document";
+	//  };
+	//
+	//  this.logDocument = function () {
+	//    var now = new Date();
+	//    return "/log/" + now.getFullYear() + "-" + (now.getMonth() + 1) + "-" + now.getDate() + ".xml";
+	//  };
+	//
+	//  this.logPath = function () {
+	//    return "doc('" + this.database + this.logDocument() + "')";
+	//  };
 
 
-  def inXml(value: String) = {
-    value.replace("<", "&lt;").replace(">", "&gt;")
-  }
+	def inXml(value: String) = {
+		value.replace("<", "&lt;").replace(">", "&gt;")
+	}
 
-  def quote(value: String) = {
-    value match {
-      case "" => "''"
-      case string =>
-        "'" + string.replace("'", "\'\'") + "'"
-    }
-  }
+	def quote(value: String) = {
+		value match {
+			case "" => "''"
+			case string =>
+				"'" + string.replace("'", "\'\'") + "'"
+		}
+	}
 
-  def execute(command: String, session: ClientSession) = session.execute(
-    s"<xquery><![CDATA[$command]]></xquery>"
-  )
+	def execute(command: String, session: ClientSession) = session.execute(
+		s"<xquery><![CDATA[$command]]></xquery>"
+	)
 
 
 }
