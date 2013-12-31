e99e57f9452486f92b683d0df16eeef127547f90
geralddejong <gerald@delving.eu>
2013-12-31 21:01:00 +0100

    Nice stamping of incoming documents

--- app/controllers/Document.scala
+++ app/controllers/Document.scala
@@ -2,11 +2,13 @@ package controllers
 
 import play.api.mvc._
 import eu.delving.basex.client._
-import org.basex.server.ClientSession
-import services.{BaseXController, BaseXConnection, BaseXBridge}
+import services.{BaseXController, BaseXConnection}
 import play.Logger
 import play.api.libs.json.JsValue
 import scala.compat.Platform
+import scala.xml.transform.{RuleTransformer, RewriteRule}
+import scala.xml.{Attribute, Text, Elem, Node}
+import scala.concurrent.Future
 
 object Document extends BaseXController {
 
@@ -48,82 +50,49 @@ object Document extends BaseXController {
     )
   }
 
-  case class HeaderFromClient(SchemaName: String, Identifier: String, Timestamp: String, SavedBy: String, Title:String)
-  case class DocumentFromClient(header: HeaderFromClient, body: JsValue, xml: String)
-
-  def saveDocument() = Action(parse.json) {
+  def saveDocument() = Action.async(parse.json) {
     request =>
-      val IDENTIFIER = "#IDENTIFIER#"
-      val TIMESTAMP = "#TIMESTAMP#"
-      val time = Platform.currentTime.toString
+      // todo: use parse.xml, send only XML and remove the next two lines
+      val legacyXmlAttribute = (request.body \ "xml").as[String]
+      val body = scala.xml.XML.loadString(legacyXmlAttribute)
+      // todo: probably don't need placeholders either
 
-      val header = (request.body \ "header").as[JsValue]
-      //        "header":{"SchemaName":"Photo","Identifier":"OSCR-Photo-d8lopli-s55","Title":"First","TimeStamp":"#TIMESTAMP#","SavedBy":"OSCR-US-d2siboo-zj6"},
-      val body = (request.body \ "body").as[JsValue]
-      //        "body":{"Photo":{"Title":"First","Type":{"Identifier":"OSCR-VO-hqebnnet-sbi","Label":"Type Two"}}},
-      val xml = (request.body \ "xml").as[String]
-      val xmlTimestamped = xml.replace("#TIMESTAMP#", time)
-      Logger.info("Document timestamped: " + xmlTimestamped)
-      // <Document>
-      // <Header> <SchemaName>Photo</SchemaName> <Identifier>OSCR-Photo-d8lopli-s55</Identifier> <Title>First</Title> <TimeStamp>#TIMESTAMP#</TimeStamp> <SavedBy>OSCR-US-d2siboo-zj6</SavedBy> </Header>
-      // <Body> <Photo> <Title>First</Title> <Type> <Identifier>OSCR-VO-hqebnnet-sbi</Identifier> <Label>Type Two</Label> </Type> </Photo> </Body>
-      // </Document>
-
-
-
-
-    
-      //      Logger.info("Header: " + header)
-      //      Logger.info("Body  : " + body)
-      //      Logger.info("XML   : " + xml)
-//      P.saveDocument = function (envelope, receiver) {
-//        var s = this.services;
-//        var IDENTIFIER = '#IDENTIFIER#';
-//        var TIMESTAMP = '#TIMESTAMP#';
-//        var time = new Date().getTime();
-//        var hdr = _.clone(envelope.header);
-//        var body = envelope.body;
-//
-//        function addDocument() {
-//          var xml = envelope.xml
-//            .replace(IDENTIFIER, hdr.Identifier) // header
-//            .replace(IDENTIFIER, hdr.Identifier) // maybe body
-//            .replace(TIMESTAMP, time); // header
-//          s.add('add document ' + hdr.Identifier,
-//          s.docDocument(hdr.SchemaName, hdr.Identifier),
-//          xml,
-//          receiver
-//          );
-//        }
-//
-//        hdr.TimeStamp = time;
-//        if (hdr.Identifier === IDENTIFIER) {
-//          if (envelope.header.SchemaName == 'MediaMetadata') {
-//            // expects fileName, mimeType
-//            log('save image');
-//            log(body);
-//            s.Media.saveMedia(body, function (fileName) {
-//              hdr.Identifier = fileName;
-//              addDocument();
-//            });
-//          }
-//          else {
-//            hdr.Identifier = util.generateDocumentId(hdr.SchemaName);
-//            addDocument();
-//          }
-//        }
-//        else {
-//          // todo: move the current one to the backup collection
-//          var stamped = envelope.xml.replace(TIMESTAMP, time);
-//          s.replace('replace document ' + hdr.Identifier,
-//          s.docDocument(hdr.SchemaName, hdr.Identifier),
-//          stamped,
-//          receiver
-//          );
-//        }
-//      };
-
-      NotImplemented
+      val ID_PLACEHOLDER = "#IDENTIFIER#"
+      object Stamp extends RewriteRule {
+        override def transform(node : Node):Node = node match {
+          case elem: Elem if elem.label == "Identifier" && elem.text == ID_PLACEHOLDER =>
+            elem.copy(child = Seq(Text(generateId("DOC"))))
+          case elem: Elem if elem.label == "TimeStamp" =>
+            elem.copy(child = Seq(Text(Platform.currentTime.toString)))
+          case remainder => remainder
+        }
+      }
+      object Stamper extends RuleTransformer(Stamp)
+      val stampedBody = Stamper(body)
+      val fresh = (body \ "Header" \ "Identifier").text == ID_PLACEHOLDER
+      val schemaName = (body \ "Header" \ "SchemaName").text
+      Logger.info("Document "+fresh+" timestamped: " + stampedBody)
+      BaseXConnection.withSession(
+        session => {
+          if (fresh) {
+            if (schemaName == "MediaMetadata") {
+              // todo: save media, and then add document
+              Future(NotImplemented)
+            }
+            else {
+              // todo: add document
+              Future(NotImplemented)
+            }
+          }
+          else {
+            // todo: replace document
+            //          s.replace('replace document ' + hdr.Identifier,
+            //          s.docDocument(hdr.SchemaName, hdr.Identifier),
+            //          stamped,
+            Future(NotImplemented)
+          }
+        }
+      )
   }
 }
 
