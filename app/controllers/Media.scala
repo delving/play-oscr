13a0911e4abaed80f5bbed05b274c2187528428c
geralddejong <gerald@delving.eu>
2014-01-08 11:34:55 +0100

    Added multipart upload action as an experiment

--- app/controllers/Media.scala
+++ app/controllers/Media.scala
@@ -2,8 +2,15 @@ package controllers
 
 import services.BaseXController
 import play.api.mvc.Action
+import play.Logger
 
 object Media extends BaseXController {
+
+  def uploadFile = Action(parse.multipartFormData) {
+    request =>
+      Logger.info("Multipart upload: " + request.body)
+      Ok("Multipart accepted")
+  }
 
   def getFile(fileName: String) = Action {
     NotImplemented
